0c312e012f222a994923d5f5196d4200294bd10a
ilham072 <86547166+Ilham072@users.noreply.github.com>
2026-01-03 23:26:56 +0800

    chore(ui): add design tokens and refactor public layout/navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,15 @@
+// tailwind.config.js
 import defaultTheme from "tailwindcss/defaultTheme";
 import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "class",
   content: [
     "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
     "./storage/framework/views/*.php",
     "./resources/views/**/*.blade.php",
-
-    // Inertia + React + TS
-    "./resources/js/**/*.js",
-    "./resources/js/**/*.ts",
-    "./resources/js/**/*.tsx",
+    "./resources/js/**/*.{js,ts,tsx}",
   ],
-
   theme: {
     extend: {
       fontFamily: {
@@ -34,6 +29,5 @@ export default {
       },
     },
   },
-
   plugins: [forms],
 };
